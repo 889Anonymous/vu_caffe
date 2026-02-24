2657be0a8b226bc8d151489ffce72cfc5c659036
innocomdev <innocomd@gmail.com>
2026-02-24 12:03:00 +0700

    refactor: Standardize project layout (src/ structure) and neutralize branding to Modern POS. Cleaned legacy Frappe/URY files.

--- ury/ury/doctype/ury_table/test_ury_table.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Tridz Technologies Pvt. Ltd. and contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestURYTable(FrappeTestCase):
-    pass
